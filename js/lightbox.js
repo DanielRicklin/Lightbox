703d508eaaf46b5ed8b64f76eff9f528108196c2
Daniel Ricklin <ricklin.daniel@outlook.fr>
2017-12-07 19:58:52 +0100

    first commit

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,40 +1,130 @@
 'use strict'
 
-let img = $('img');
-let imgCourante, nextDiv, nextImg, nextH1, nbImage;
+$(document).ready(function () {
 
-$(img).click(function () {
-    $(".gallery-container").after($("<img>").attr('src', this.attributes[0].value).attr('class', 'pe'));
-    $(".gallery-container").after($("<button>").text("X").attr('class', 'close'));
-    $(".gallery-container").after($("<button>").text("<<").attr('class', 'move prev'));
-    $(".gallery-container").after($("<button>").text(">>").attr('class', 'move next'));
+    let lightbox = {};
+    lightbox.modules = {};
 
-    imgCourante = this;
-    let titre = $(imgCourante).parent().find("div");
-    $(".gallery-container").after($("<h1>").text(titre.text()).attr('class', 'titre'));
-    for (let i = 0; i < img.length; i++) {
-        if ($(imgCourante).attr('data-img') == img[i]['attributes'][0]['value']) {
-            nbImage = i;
+    lightbox.modules.action = (function () {
+        let html = [];
+
+        let create = function (id, titre) {
+            html.push('<div class="gallery">');
+            html.push("<img class='pe' src='" + id + "'>");
+            html.push("<h1 class='titre'>" + titre + "</h1>");
+            html.push("<button class='close'>X</button>");
+            html.push("<button class='move prev'><<</button>");
+            html.push("<button class='move next'>>></button>");
+            html.push("<div class='commentary'><button class='addCommentary'>Add commentary</button><button class='erase'>Erase</button><div class='comList'></div></div>");
+            html.push('</div>');
         }
-    }
-});
 
-$(document).on('click', '.next', function () {
-    $(".pe").remove();
-    $(".titre").remove();
-    nextDiv = $(imgCourante).parent().next();
-    nextH1 = nextDiv.find("div");
-    nextImg = nextDiv.find("img");
-    //let srcImg = $(img[i+1]);
-    //console.log(nextDiv);
-    //console.log(nextImg);
-    $(".gallery-container").after($("<img>").attr('src', nextImg.attr('data-img')).attr('class', 'pe'));
-    $(".gallery-container").after($("<h1>").text(nextH1.text()).attr('class', 'titre'));
-});
+        let listeners = function (event) {
+            $(".close").on('click', (e) => {
+                hide();
+            });
+            $(".prev").on('click', (e) => {
+                prev(event);
+            });
+            $(".next").on('click', (e) => {
+                next(event);
+            });
 
-$(document).on('click', '.close', function () {
-    $(".pe").remove();
-    $('.close').remove();
-    $('.move').remove();
-    $('.titre').remove();
+            $(document).keydown(function (e) {
+                switch (e.which) {
+                    case 37:
+                        prev(event);
+                    case 39:
+                        next(event);
+                }
+            });
+
+            $(".addCommentary").on('click', (e) => {
+                getCom(event);
+            });
+            $(".erase").on('click', (e) => {
+                eraseCom(event);
+            });
+        }
+
+        let show = function () {
+            $(html.join('')).appendTo('body');
+        }
+
+        let hide = function () {
+            $(".gallery").remove();
+            html = [];
+        }
+
+        let prev = function (e) {
+            let id = $(e.target).parent().prev().children().data('img');
+            let titre = $(e.target).parent().prev().children().next().text();
+
+            if (typeof id !== 'undefined') {
+                hide();
+                create(id, titre);
+                show();
+                var obj = {};
+                obj.target = $(e.target).parent().prev().children();
+                listeners(obj);
+                showCommentary(obj);
+            }
+        }
+
+        let next = function (e) {
+            let id = $(e.target).parent().next().children().data('img');
+            let titre = $(e.target).parent().next().children().next().text();
+
+            if (typeof id !== 'undefined') {
+                hide();
+                create(id, titre);
+                show();
+                var obj = {};
+                obj.target = $(e.target).parent().next().children();
+                listeners(obj);
+                showCommentary(obj);
+            }
+        }
+
+        let getCom = function (eventimg) {
+            let id = $(eventimg.target).data('img');
+            let com = prompt("Votre commentaire");
+            if (localStorage.getItem(id)) {
+                com = "<span>" + com + "</span> / " + localStorage.getItem(id);
+            } else {
+                com = "<span>" + com + "</span> " + localStorage.getItem(id);
+            }
+            localStorage.setItem(id, com);
+            showCommentary(eventimg);
+        }
+
+        let showCommentary = function (e) {
+            let id = $(e.target).data('img');
+            $('.comList').text('');
+            $(localStorage.getItem(id)).appendTo('.comList');
+        }
+
+        let eraseCom = function (eventimg) {
+            let id = $(eventimg.target).data('img');
+            localStorage.setItem(id, '');
+            showCommentary(eventimg);
+        }
+
+        let start = function () {
+            $("img").on('click', (event) => {
+                let id = $(event.target).data('img');
+                let titre = $(event.target).next().text();
+                create(id, titre);
+                show();
+                listeners(event);
+                showCommentary(event);
+            });
+        }
+
+        return {
+            start
+        };
+
+    })();
+    lightbox.modules.action.start();
 });
